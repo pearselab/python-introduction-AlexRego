44019212c3edf9c90205b8edf768cecbb458187b
AlexRego <alexandre.rego@aggiemail.usu.edu>
2016-11-16 11:36:02 -0700

    crass class

--- intro.py
+++ intro.py
@@ -1,5 +1,7 @@
+import math
+
 # 1.
-for i in range(20,9,-1):
+for i in range(20, 9, -1):
     print(i)
 
 # 2.
@@ -7,14 +9,13 @@ def build_reverse(): return [x for x in range(20,9,-1)]
 
 # 3.
 for i in range(20,9,-1):
-    if (i % 2 == 0):
+    if i % 2 == 0:
         print(i)
 
 # 4.
 def build_reverse_even(): return [x for x in range(20,9,-1) if x%2 ==0]
 
 # 5.
-import math
 
 def is_prime(n):
     if n < 2:
@@ -32,7 +33,7 @@ with open('file_name') as handle:
         print(line[5])
 
 # 7.
-for i in range(1,21):
+for i in range(1, 21):
     if i % 5 == 0:
         print('Good:', i)
     if is_prime(i):
@@ -61,7 +62,8 @@ class point:
 # 11
 class point:
     def __init__(self, x1, y1, x2, y2):
-        self.x = x, self.y = y
+        self.x, self.y
+
     def dist(x1,y1,x2,y2):
         math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
