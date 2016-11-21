b6bf832eb26a35675c24d28b017881e9b54ef435
AlexRego <alexandre.rego@aggiemail.usu.edu>
2016-11-21 10:45:57 -0700

    sorting.py

--- sorting.py
+++ sorting.py
@@ -3,8 +3,16 @@ def bubblesort(nums):
     for n in range(len(nums), 0, -1):
         for i in range(n):
             if nums[i] < nums[i+1]:
-                nums[i], nums[index +1] = nums[i+1], nums[i]
+                nums[i], nums[i +1] = nums[i+1], nums[i]
     return nums
 
 ## Quick sort
-def quicksort(nums, bottom, top)
+def quicksort(nums):
+    bottom = 0
+    top = len(nums)
+
+## Partition
+def partition(list):
+    bottom = 0
+    top = len(nums)
+    pivot = nums[top]
